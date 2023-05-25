829fa6f95b09e4a74b0f80dd2cc50dd6fd173691
Matthew Klemmer <theklemmercode@outlook.com>
2023-05-25 10:14:30 -0400

    Updated about me.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ function Main() {
       <h3>About</h3>
       <p>I am a software engineer deeply interested in creating solutions for real world problems and automating the mundane. I like to keep up with security and best practices, and I’m always looking for new things to learn.</p>
       <h3>Interests</h3>
-      <p>Avid Music Listener. Tech Junkie. Avid Book Reader. Semi-Proficient Gamer. Coffee Enjoyer. Miniature Painter.</p>
+      <p>Avid Music Listener. Tech Junkie. Book Reader. Coffee Enjoyer. Miniature Painter.</p>
     </main>
   )
 }
